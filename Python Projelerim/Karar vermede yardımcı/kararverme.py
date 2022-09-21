7167b9b142176bebb7c4cb675c9faba1a66703c0
idiill <111890733+idiill@users.noreply.github.com>
2022-09-21 14:16:19 +0300

    Update kararverme.py

--- Python Projelerim/Karar vermede yardımcı/kararverme.py
+++ Python Projelerim/Karar vermede yardımcı/kararverme.py
@@ -1,13 +1,16 @@
-
 import random 
 a=int(input("Kararsız kaldığınız kaç durum var: "))
   
 sayac=0
-durumlar=[]       
-for i in range (sayac,a): 
-    c=input(f"Kararsız kaldığınız {sayac+1}.durumu giriniz: ")
-    durumlar.append(c)
-    sayac+=1
+durumlar=[] 
 
-secimi_yap=random.choice(durumlar)
-print(f"Yapılan seçim: {secimi_yap}")
+if a==1:
+    print("Seçim yapılabilmesi için lütfen en az iki durum giriniz.") 
+    
+else:
+    for i in range (sayac,a): 
+        c=input(f"Kararsız kaldığınız {sayac+1}.durumu giriniz: ")
+        durumlar.append(c)
+        sayac+=1
+    secimi_yap=random.choice(durumlar)
+    print(f"Yapılan seçim: {secimi_yap}")

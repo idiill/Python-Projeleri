0bec930c02bb7cd7bb69a4bafe9e1fef85e0b73c
idiill <111890733+idiill@users.noreply.github.com>
2022-09-22 22:04:35 +0300

    Update kararverme.py

--- Python Projelerim/Karar vermede yardımcı/kararverme.py
+++ Python Projelerim/Karar vermede yardımcı/kararverme.py
@@ -1,8 +1,24 @@
-import random 
+import random,time,sys
+
+
+def exit():
+    print("*"*32)
+    print("Karar veriliyor")
+    time.sleep(.5)
+    print(".")
+    time.sleep(.5)
+    print("..")
+    time.sleep(.5)    
+    print("...")
+    sys.exit()
+
+
+
 a=int(input("Kararsız kaldığınız kaç durum var: "))
   
 sayac=0
 durumlar=[] 
+
 
 if a==1:
     print("Seçim yapılabilmesi için lütfen en az iki durum giriniz.") 
@@ -13,4 +29,6 @@ else:
         durumlar.append(c)
         sayac+=1
     secimi_yap=random.choice(durumlar)
+    exit()
     print(f"Yapılan seçim: {secimi_yap}")
+    
